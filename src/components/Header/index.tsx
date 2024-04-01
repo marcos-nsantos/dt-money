b67cc532816e6ef7a9b66409a6315e320dc21cf8
Marcos Santos <89700349+marcos-nsantos@users.noreply.github.com>
2024-03-31 20:45:10 -0500

    feat: add NewTransactionModal component and styles
    This commit introduces the NewTransactionModal component with relevant styles for creating new transactions. In addition, it includes updates on the package lock file to add new dependencies required. '@phosphor-icons/react', '@radix-ui/react-dialog' and custom styles are used within this component to create a form for transaction attributes such as description, price and category.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,14 +3,23 @@ import {
   HeaderContent,
   NewTransactionButton,
 } from "./styles.ts";
+import * as Dialog from "@radix-ui/react-dialog";
 import logoImg from "../../assets/logo.svg";
+import { NewTransactionModal } from "../NewTransactionModal";
 
 export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
         <img src={logoImg} alt="" />
-        <NewTransactionButton>Nova transação</NewTransactionButton>
+
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+
+          <NewTransactionModal />
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   );
