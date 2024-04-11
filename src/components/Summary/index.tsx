a1543547eb47bf654ea68357f7acc7a4a863babd
Marcos Santos <89700349+marcos-nsantos@users.noreply.github.com>
2024-04-10 19:57:00 -0500

    refactor: add useSummary hook for transaction summaries
    Move the transaction summary logic from the Summary component to a new custom hook called useSummary. This simplifies the Summary component and centralizes the computation of transaction summaries in one place in the code base.

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -4,31 +4,11 @@ import {
   ArrowCircleUp,
   CurrencyDollar,
 } from "@phosphor-icons/react";
-import { useContext } from "react";
-import { TransactionContext } from "../../context/TransactionContext.tsx";
 import { priceFormatter } from "../../utils/formatter.ts";
+import { useSummary } from "../../hook/useSummary.ts";
 
 export function Summary() {
-  const { transactions } = useContext(TransactionContext);
-
-  const summary = transactions.reduce(
-    (acc, transaction) => {
-      if (transaction.type === "income") {
-        acc.income += transaction.price;
-        acc.total += transaction.price;
-      } else {
-        acc.outcome += transaction.price;
-        acc.total -= transaction.price;
-      }
-
-      return acc;
-    },
-    {
-      income: 0,
-      outcome: 0,
-      total: 0,
-    },
-  );
+  const summary = useSummary();
 
   return (
     <SummaryContainer>
